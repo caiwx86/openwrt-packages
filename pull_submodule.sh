9e59d942b4bdd70b67aa058b4192324a1ac33959
caiwx86 <caiwenxiu0806@163.com>
2024-10-06 22:47:37 +0800

    update

--- pull_submodule.sh
+++ pull_submodule.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 git clone https://github.com/kenzok8/openwrt-packages tmp/openwrt-packages
 git clone https://github.com/kenzok8/small tmp/small
-git rm -rf openwrt-packages small
-mkdir openwrt-packages small
-cp -rv tmp/openwrt-packages/* openwrt-packages
-cp -rv tmp/small/* small 
+https://github.com/kenzok8/small-package tmp/small-package
+git rm -rf openwrt-packages small small-package
+mkdir openwrt-packages small small-package
+cp -r tmp/openwrt-packages/* openwrt-packages
+cp -r tmp/small/* small 
+cp -r tmp/small-package/* small-package 
 git add -A openwrt-packages
 git add -A small
+git add -A small-package
 git commit -m "update"
 git push -f
