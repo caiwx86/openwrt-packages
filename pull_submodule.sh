509e54b1e3a3937339ba74e30461a06ff16df0c3
caiwx86 <caiwenxiu0806@163.com>
2024-12-18 00:22:12 +0800

    update

--- pull_submodule.sh
+++ pull_submodule.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
-git clone https://github.com/kenzok8/openwrt-packages tmp/openwrt-packages
-git clone https://github.com/kenzok8/small tmp/small
-git clone https://github.com/kenzok8/small-package tmp/small-package
-git rm -rf openwrt-packages small small-package
-mkdir openwrt-packages small small-package
-cp -r tmp/openwrt-packages/* openwrt-packages
-cp -r tmp/small/* small 
-cp -r tmp/small-package/* small-package 
-git add -A openwrt-packages
-git add -A small
-git add -A small-package
+# Git稀疏克隆，只克隆指定目录到本地
+function git_update() {
+  repourl="$1" repodir="$2" && shift 2
+  if [ -d "$repodir" ]; then
+    cd $repodir && git pull
+  else
+    git clone --depth=1 $repourl $repodir
+  fi
+
+  final_dir=${repodir##*/}
+  git rm -rf $final_dir
+  mkdir $final_dir
+  cp -r $repodir/* $final_dir
+  git add -A $final_dir
+}
+
+git_update https://github.com/kenzok8/openwrt-packages tmp/openwrt-packages
+git_update https://github.com/kenzok8/small tmp/small
+git_update https://github.com/kenzok8/small-package tmp/small-package
+git_update https://github.com/danchexiaoyang/luci-app-onliner tmp/other
 git commit -m "update"
 git push -f
